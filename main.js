2ac11dd22ae98a002be60f677724de099bc3d963
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-04 10:06:09 -0400

    corrected functionality on main.js and views files, enabled each name to be clicked and redirected to an individual profile

---
+++ main.js
@@ -0,0 +1,45 @@
+// // =================Packages=====================
+
+const express = require('express');
+const mustacheExpress = require('mustache-express')
+const data = require('./data')
+const fs = require('fs')
+const app = express();
+
+// ======Boilerplate, Calling modules to be used======
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+// ========Retrieving robot info from database========
+
+app.get('/', function(req, res) {
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].job === null) {
+      data.users[i].job = "Available for hire";
+    }
+  }
+  res.render('home', {
+    users: data.users
+  })
+});
+
+app.get('/:profile', function(req, res) {
+  let robot = req.params.profile;
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username === robot) {
+      res.render('profile', {
+        users: data.users[i]
+      });
+    }
+  }
+});
+
+console.log(data);
+
+app.listen(3000, function() {
+  console.log('App is running!');
+});
